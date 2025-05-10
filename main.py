7814bbf3da36adeebb758da067de5e0dad785f82
Kostia <kostiantyn.tel@gmail.com>
2025-05-10 18:02:23 +0300

    v1000000

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, math
+import pygame, math, json
 from map import *
 pygame.init()
 
@@ -15,8 +15,20 @@ pygame.display.set_icon(pygame.image.load("images/ground.png"))
 bg = pygame.image.load("images/bg.png")
 bg = pygame.transform.scale(bg, (width, height))
 music = pygame.mixer.music.load("sounds/main_music.mp3")
+coin_sound = pygame.mixer.Sound("sounds/coin_sound.wav")
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)
+
+try:
+    with open("data.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+except:
+    with open("data.json", "w", encoding="utf-8") as file:
+        data = {"lvl": "0", "coins": "0"}
+        json.dump(data, file)
+
+score = int(data["coins"])
+lvl = int(data["lvl"])
 
 class Sprite:
     def __init__(self , x , y , w , h, img):
@@ -33,6 +45,7 @@ class Sprite:
 class Player(Sprite):
     def __init__(self , x , y , w , h , img1, img2 , speed, jumpforce):
         super().__init__(x, y, w, h, img1)
+        global num_of_hearts
         self.img_l = self.img
         self.img_r = pygame.transform.scale(img2, (w, h))
         self.speed = speed
@@ -42,7 +55,7 @@ class Player(Sprite):
         self.gravity = 0.2
         self.max_fall_speed = 10
         self.CanJump = False
-        self.hp = 5
+        self.hp = num_of_hearts
         self.start_x, self.start_y = x, y
 
     def move(self, plats):
@@ -99,10 +112,23 @@ class Player(Sprite):
             self.rect.x, self.rect.y = self.start_x, self.start_y
             camera.rect.x, camera.rect.y = 0, 0
         else:
+            fade_in(window, width, height)
             menu = True
     
     def fire(self, pos):
         bullets.append(Bullet(self.rect.centerx + 20,self.rect.y - 15, 10, 10, pygame.image.load("images/bullet.png"), 10, pos))
+
+class Enemy(Sprite):
+    def __init__(self , x , y , w , h , img1 , speed, range):
+        super().__init__(x, y, w, h, img1)
+        self.speed = speed
+        self.start_x = x
+        self.finish_x = self.start_x + range
+    
+    def move(self):
+        self.rect.x += self.speed
+        if self.rect.x > self.finish_x or self.rect.x < self.start_x:
+            self.speed *= -1
 
 class Lift(Sprite):
     def __init__(self, w, h, img, speed, x1, x2, y1, y2, type):
@@ -163,12 +189,12 @@ class Bullet(Sprite):
 class Portal(Sprite):
     def __init__(self, x, y, w, h, img):
         super().__init__(x, y, w, h, img)
-        self.wait = 30
+        self.wait = 50
     
     def teleport(self, player: Player, p2):
         if self.wait == 0:
             player.rect.x, player.rect.y = p2.rect.x, p2.rect.y - player.rect.height
-            p2.wait = 30
+            p2.wait = 50
 
     def reset(self):
         if self.wait > 0:
@@ -186,6 +212,23 @@ class Spike(Sprite):
             self.rect.x = self.start_x + math.sin(i) * len
         elif self.type == 'moving vertical':
             self.rect.y = self.start_y + math.sin(i) * len
+
+class Mlyn(Sprite):
+    def __init__(self, x, y, w, h, img):
+        super().__init__(x, y, w, h, img)
+        self.original_img = self.img.copy()
+        self.angle = 0
+
+    def update(self):
+        self.angle = (self.angle + 2) % 360
+        self.img = pygame.transform.rotate(self.original_img, self.angle)
+        center = self.rect.center
+        self.img = pygame.transform.scale(self.img, (self.rect.w, self.rect.h))
+        self.rect = self.img.get_rect(center=center)
+
+    def draw(self):
+        window.blit(self.img, (self.rect.x - camera.rect.x, self.rect.y - camera.rect.y))
+        
 camera = Camera(0, 0, width, height, 2)
 
 block_img = pygame.image.load("images/ground.png")
@@ -201,15 +244,22 @@ bullets = []
 hearts = []
 portals = []
 coins = []
+mlyns = []
+finishs = []
+enemies = []
+s_blocks = []
 
 def load_lvl(lvl):
-    global block_x, block_y, block_size, blocks, lifts, spikes, portals, coins
+    global block_x, block_y, block_size, blocks, lifts, spikes, portals, coins, mlyns, finishs, enemies, s_blocks
     block_x, block_y = 0, -600
     blocks = []
     lifts = []
     spikes = []
     portals = []
     coins = []
+    mlyns = []
+    finishs = []
+    enemies = []
     for row in lvl:
         for tile in row:
             if tile == 1:
@@ -234,28 +284,53 @@ def load_lvl(lvl):
                 spikes.append(Spike(block_x, block_y, block_size, block_size, pygame.image.load("images/spike2.png")))
             elif tile == 5:
                 portals.append(Portal(block_x, block_y, block_size/2, block_size, pygame.image.load("images/portal.png")))
+            elif tile == 8:
+                mlyns.append(Mlyn(block_x, block_y, block_size*2, block_size*2, pygame.image.load("images/mlyn.png")))
+            elif tile == 9:
+                finishs.append(Sprite(block_x, block_y, block_size, block_size*2, pygame.image.load("images/door.png")))
+            elif tile == 10:
+                enemies.append(Enemy(block_x, block_y, block_size, block_size, pygame.image.load("images/enemy.png"), 3, 560))
+            elif tile == 20:
+                s_blocks.append(Sprite(block_x, block_y, block_size, block_size, pygame.image.load("images/wall2.png")))
 
             block_x += block_size
         block_x = 0
         block_y += block_size
 
-load_lvl(lvl2)
+def fade_in(screen, width, height):
+    fade_s = pygame.Surface((width, height))
+    fade_s.fill((0, 0, 0))
+    for a in range(0, 255, 5):
+        fade_s.set_alpha(a)
+        screen.blit(fade_s, (0, 0))
+        pygame.display.update()
+        pygame.time.delay(30)
+
+def save():
+    with open("data.json", "w", encoding="utf-8") as file:
+        json.dump(data, file)
+
+load_lvl(lvls[lvl])
 
 for i in range(num_of_hearts):
     hearts.append(Sprite(i * 50, 0, 50, 50, pygame.image.load("images/heart.png")))
 
-pl_img = pygame.image.load("images/player.png")
-player = Player(50, 490, 30, 70, pl_img, pygame.transform.flip(pl_img, True, False), 2, 12)
+pl_img = pygame.image.load("images/Player.png")
+player = Player(50, 490, 30, 70, pl_img, pygame.transform.flip(pl_img, True, False), 2.5, 12)
 gun = Sprite(player.rect.x + 10, player.rect.y - 15, 20, 50, pygame.image.load("images/gun.png"))
 play_btn = Sprite(width/2-100, height/2-50, 200, 100, pygame.image.load("images/btns/Play_btn.png"))
 menu_btn = Sprite(width-200, 0, 200, 100, pygame.image.load("images/btns/Menu_btn.png"))
 exit_btn = Sprite(0, height-75, 150, 75, pygame.image.load("images/btns/Exit_btn.png"))
+settings_btn = Sprite(0, 0, 300, 75, pygame.image.load("images/btns/Settings_btn.png"))
+slider = Sprite(300, 193, 200, 15, pygame.image.load("images/slider.png"))
 font = pygame.font.Font("fonts/Macondo-Regular.ttf", 50)
 play_txt = font.render("PLAY", True, (0, 0, 0))
 menu_txt = font.render("MENU", True, (0, 0, 0))
 exit_txt = font.render("EXIT", True, (0, 0, 0))
+settings_txt = font.render("SETTINGS", True, (0, 0, 0))
 
 menu = True
+settings = False
 game = True
 while game:
     window.blit(bg, (0, 0))
@@ -277,8 +352,14 @@ while game:
                 game = False
             if menu_btn.rect.collidepoint(pos[0], pos[1]) and not menu:
                 menu = True
+                settings = False
+            if settings_btn.rect.collidepoint(pos[0], pos[1]) and menu:
+                menu = False
+                settings = True
     
     if not menu:
+        coins_txt = font.render(f"Coins: {score}", True, (0, 0, 0))
+        window.blit(coins_txt, (0, 80))
         if CanShoot:
             gun.draw()
             if player.img == player.img_r:
@@ -293,16 +374,26 @@ while game:
                     score += 50
                 else:
                     score += 1
+                coin_sound.play(0)
                 coins.remove(c)
         
         for h in hearts:
-            h.draw()
-            h.rect.y = camera.rect.y
-            h.rect.x = camera.rect.x + hearts.index(h)*50
+            h.draw_ui()
         
         for spike in spikes:
             spike.draw()
             spike.move(phase, 20)
+        
+        for m in mlyns:
+            m.update()
+            m.draw()
+        
+        for f in finishs:
+            f.draw()
+        
+        for e in enemies:
+            e.draw()
+            e.move()
         
         for p in portals:
             p.draw()
@@ -318,20 +409,29 @@ while game:
         
         for block in blocks:
             block.draw()
+            
+        for b in s_blocks:
+            b.draw()
+            if player.rect.colliderect(b.rect):
+                s_blocks.remove(b)
         
-        if any(player.rect.colliderect(s.rect) for s in spikes):
+        if any(player.rect.colliderect(s.rect) for s in spikes) or any(player.rect.colliderect(m.rect) for m in mlyns) or any(player.rect.colliderect(e.rect) for e in enemies):
             player.damage()
+        
+        if any(player.rect.colliderect(f.rect) for f in finishs):
+            lvl += 1
+            fade_in(window, width, height)
+            try:
+                load_lvl(lvls[lvl])
+                player.rect.x, player.rect.y = 50, 490
+                camera.rect.x, camera.rect.y = 0, 0
+            except:
+                menu = True
         
         player.draw()
         player.move(blocks)
         player.jumping(blocks, lifts)
         menu_btn.draw_ui()
-        
-        pos = pygame.mouse.get_pos()
-        if menu_btn.rect.collidepoint(pos[0], pos[1]):
-            menu_btn.img = pygame.transform.scale(pygame.image.load("images/btns/Menu_btn2.png"), (menu_btn.rect.w, menu_btn.rect.h))
-        else:
-            menu_btn.img = pygame.transform.scale(pygame.image.load("images/btns/Menu_btn.png"), (menu_btn.rect.w, menu_btn.rect.h))
             
         window.blit(menu_txt, (width - 180, 30))
         for i in range(len(portals)):
@@ -340,7 +440,6 @@ while game:
                     portals[i].teleport(player, portals[j])
         camera.move(player)
         phase += 0.008
-        print(len(spikes))
 
     if menu:
         window.blit(bg, (0, 0))
@@ -358,9 +457,10 @@ while game:
             exit_btn.img = pygame.transform.scale(pygame.image.load("images/btns/Exit_btn2.png"), (exit_btn.rect.w, exit_btn.rect.h))
         else:
             exit_btn.img = pygame.transform.scale(pygame.image.load("images/btns/Exit_btn.png"), (exit_btn.rect.w, exit_btn.rect.h))
-            
+        
     
     pygame.display.update()
     clock.tick(60)
 
 pygame.quit()
+save()
